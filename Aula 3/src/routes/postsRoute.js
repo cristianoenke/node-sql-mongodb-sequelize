8f77cf0a519cb08e23f8e90385b346e2bfb1608a
unknown <cristiano.enke@gmail.com>
2021-11-01 09:39:43 -0300

    Node with Hapi

---
+++ Aula 3/src/routes/postsRoute.js
@@ -0,0 +1,52 @@
+import Joi from 'joi'
+
+import PostsController from '../controllers/posts'
+import PostsModel from '../models/posts'
+
+const postsController = new PostsController(PostsModel)
+
+const postsRoute = (server) => {
+    server.route({
+        method: 'GET',
+        path: '/posts/{id?}', 
+        handler: (request) => postsController.find(request)
+    })
+
+    server.route({
+        method: 'POST',
+        path: '/posts', 
+        handler: (request,h) => postsController.create(request,h),
+        options: {
+            validate: {
+                payload: {
+                    title: Joi.string().required(),
+                    content: Joi.string().required(),
+                    author: Joi.string().required(),
+                }
+            }
+        }
+    })
+
+    server.route({
+        method: 'PUT',
+        path: '/posts/{id}', 
+        handler: (request,h) => postsController.update(request,h),
+        options: {
+            validate: {
+                payload: {
+                    title: Joi.string(),
+                    content: Joi.string(),
+                    author: Joi.string(),
+                }
+            }
+        }
+    })
+
+    server.route({
+        method: 'DELETE',
+        path: '/posts/{id}', 
+        handler: (request,h) => postsController.delete(request,h)
+    })
+}
+
+module.exports = postsRoute
